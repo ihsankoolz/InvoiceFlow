50aa1c3aca0342576ea886529bd6f8413860fe13
amandaseah <amanda.seah.2024@smu.edu.sg>
2026-03-17 00:36:27 +0800

    feat: added date filtering in activity log

--- services/activity-log-bridge/bridge.py
+++ services/activity-log-bridge/bridge.py
@@ -3,23 +3,22 @@ import requests
 import json
 from datetime import datetime
 
-# print("!!! SCRIPT IS STARTING NOW !!!") 
+# print("SCRIPT IS STARTING NOW !!!") 
 
-# 1. YOUR CONFIGURATION
-# Replace this with your actual OutSystems API URL from Postman
+# CONFIGURATION
 OUTSYSTEMS_URL = "https://personal-xowqm3b2.outsystemscloud.com/ActivityLog/rest/ActivityLogAPI/LogEvent"
 
 def relay_to_outsystems(ch, method, properties, body):
     try:
-        # Parse the incoming RabbitMQ message
+        # parse the incoming RabbitMQ message
         payload_dict = json.loads(body)
         event_type = method.routing_key
         
-        # 2. PREPARE THE DATA FOR OUTSYSTEMS
-        # We map RabbitMQ data to your OutSystems 'EventRequest' structure
+        # PREPARE THE DATA FOR OUTSYSTEMS
+        # map RabbitMQ data to OutSystems 'EventRequest' structure
         event_data = {
             "event_type": event_type,
-            "payload": json.dumps(payload_dict), # Store the whole JSON as a string
+            "payload": json.dumps(payload_dict), # store the whole JSON as a string
             "source_service": payload_dict.get("source", "RabbitMQ_Bridge"),
             "timestamp": datetime.now().isoformat(),
             "invoice_token": payload_dict.get("invoice_token", "N/A"),
@@ -27,7 +26,7 @@ def relay_to_outsystems(ch, method, properties, body):
             "severity": "INFO"
         }
 
-        # 3. SEND TO OUTSYSTEMS
+        # SEND TO OUTSYSTEMS
         response = requests.post(OUTSYSTEMS_URL, json=event_data)
         
         if response.status_code == 200:
@@ -38,20 +37,19 @@ def relay_to_outsystems(ch, method, properties, body):
     except Exception as e:
         print(f" [FAILED] Could not process message: {e}")
 
-# 4. CONNECT TO RABBITMQ
-# Since you're running this inside the InvoiceFlow docker network eventually, 
+# CONNECT TO RABBITMQ 
 # 'localhost' works for local testing, 'rabbitmq' works for Docker.
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq'))
 channel = connection.channel()
 
-# Declare the exchange (must match your project config)
+# declare the exchange 
 channel.exchange_declare(exchange='invoiceflow_events', exchange_type='topic')
 
-# Create a temporary queue for this bridge
+# create a temporary queue for bridge
 result = channel.queue_declare(queue='outsystems_audit_queue', durable=True)
 queue_name = result.method.queue
 
-# Bind the queue to EVERYTHING (the # wildcard)
+# bind the queue to everything
 channel.queue_bind(exchange='invoiceflow_events', queue=queue_name, routing_key='#')
 
 print(' [*] Bridge is active. Listening for RabbitMQ events. To exit press CTRL+C')
