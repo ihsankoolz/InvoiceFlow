3a55bd6bea43c97130c674272d2c9608a12942af
Michelle Tan <michelletan.2024@smu.edu.sg>
2026-04-02 15:52:56 +0800

    Update test_loan_maturity_workflow.py

--- temporal-worker/tests/test_loan_maturity_workflow.py
+++ temporal-worker/tests/test_loan_maturity_workflow.py
@@ -17,7 +17,6 @@ import pytest
 from temporalio import activity
 from temporalio.testing import WorkflowEnvironment
 from temporalio.worker import Worker
-
 from workflows.loan_maturity import LoanMaturityWorkflow
 
 
